17752bae50d8dcfb1c2c4cecaa4000b072c5fc7f
Léo Geison <leogeisom@gmail.com>
2023-05-19 14:16:09 -0300

    created error handling middleware

--- src/server.js
+++ src/server.js
@@ -1,11 +1,29 @@
-const express = require('express')
+require('express-async-errors');
 
-const routes = require('./routes')
+const AppError = require('./utils/AppError');
+const express = require('express');
 
-const app = express()
-app.use(express.json())
+const routes = require('./routes');
 
-app.use(routes)
+const app = express();
+app.use(express.json());
 
-const PORT = 3333
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.use(routes);
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  });
+});
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
