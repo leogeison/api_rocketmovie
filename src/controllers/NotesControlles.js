142fafd816696c46483e830a7508cb5aa80ae76f
Léo Geison <leogeisom@gmail.com>
2023-05-29 21:25:51 -0300

    Add functionality to view and remove notes

--- src/controllers/NotesControlles.js
+++ src/controllers/NotesControlles.js
@@ -23,6 +23,25 @@ class NotesController {
 
     response.json();
   }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const note = await knex('movie_notes').where({ id }).first();
+    const tags = await knex('movie_tags')
+      .where({ note_id: id })
+      .orderBy('name');
+
+    return response.json({ ...note, tags });
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex('movie_notes').where({ id }).delete();
+
+    return response.json();
+  }
 }
 
 module.exports = NotesController;
